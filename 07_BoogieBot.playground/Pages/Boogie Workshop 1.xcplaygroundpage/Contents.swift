30e86dd0e3659d22af3279472859a3e6918104a1
David Dinh <david.dinh@medmen.com>
2018-12-09 22:46:39 -0800

    lesson 7 completed

---
+++ 07_BoogieBot.playground/Pages/Boogie Workshop 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+/*:
+ ## Boogie Workshop
+ 
+ This page is here for you to create your own routines. 
+ 
+ Remember the moves:
+ 
+ - `leftArmUp()`, `leftArmDown()`, `rightArmUp()`, `rightArmDown()`
+ - `leftLegUp()`, `leftLegDown()`, `rightLegUp()`, `rightLegDown()`
+ - `shakeItLeft()`, `shakeItRight()`, `shakeItCenter()`
+ - `jumpUp()`, `jumpDown()`
+ - `fabulize()`, `defabulize()`
+ 
+ To sign your work:
+ 
+ `setBotTitle("My Awesome Dance")`\
+ `setBotSubtitle("By The Boogiemaster")`
+*/
+startBot()
+startRecording()
+
+func boogie() {
+    fabulize()
+    leftArmUp()
+    rightArmUp()
+    shakeItLeft()
+    shakeItRight()
+    leftArmDown()
+    rightArmDown()
+    leftLegUp()
+    rightLegUp()
+    leftLegDown()
+    rightLegDown()
+}
+
+boogie()
+
+
+
+
+
+
+
+
+//: Continue with your creativity.
+//:
+//:[Previous](@previous)  |  page 10 of 13  |  [Next: Boogie Workshop](@next)
---
+++ 07_BoogieBot.playground/Pages/Boogie Workshop 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,50 @@
+/*:
+ ## Boogie Workshop
+ 
+ This page is here for you to create your own routines.
+ 
+ Remember the moves:
+ 
+ - `leftArmUp()`, `leftArmDown()`, `rightArmUp()`, `rightArmDown()`
+ - `leftLegUp()`, `leftLegDown()`, `rightLegUp()`, `rightLegDown()`
+ - `shakeItLeft()`, `shakeItRight()`, `shakeItCenter()`
+ - `jumpUp()`, `jumpDown()`
+ - `fabulize()`, `defabulize()`
+ 
+ To sign your work:
+ 
+ `setBotTitle("My Awesome Dance")`\
+ `setBotSubtitle("By The Boogiemaster")`
+ */
+startBot()
+startRecording()
+
+func boogie2() {
+    setBotTitle("My Awesome Dance")
+    setBotSubtitle("By The Boogiemaster")
+    fabulize()
+    leftArmUp()
+    rightArmUp()
+    shakeItLeft()
+    shakeItRight()
+    leftArmDown()
+    rightArmDown()
+    leftLegUp()
+    rightLegUp()
+    leftLegDown()
+    rightLegDown()
+}
+
+boogie2()
+
+
+
+
+
+
+
+
+
+//: Continue with your creativity.
+//:
+//:[Previous](@previous)  |  page 11 of 13  |  [Next: Boogie Workshop](@next)
