9f0afdfb78ccd67f7877e98eb36f01c8f6de145d
David Dinh <david.dinh@medmen.com>
2018-12-10 21:08:09 -0800

    completed lsn 10

--- 10_ParametersAndResults.playground/Pages/Exercise-Using Return Values.xcplaygroundpage/Contents.swift
+++ 10_ParametersAndResults.playground/Pages/Exercise-Using Return Values.xcplaygroundpage/Contents.swift
@@ -5,16 +5,14 @@
 
  The function `impossibleBeliefsCount` takes several numbers of reported unlikely incidents. It then prints the number of impossible things to be believed:
 */
-func impossibleBeliefsCount(pigsFlying: Int, frogsBecomingPrinces: Int, multipleLightningStrikes: Int) {
+func impossibleBeliefsCount(pigsFlying: Int, frogsBecomingPrinces: Int, multipleLightningStrikes: Int) -> Int {
     let total = pigsFlying + frogsBecomingPrinces + multipleLightningStrikes
-    print(total)
+    return total
 }
 //: - callout(Exercise):Update the `impossibleBeliefsCount` function so that instead of printing the value, it returns it as an `Int`.
 //:
 //: `impossibleThingsPhrase` creates a phrase using string interpolation:
-func impossibleThingsPhrase() -> String {
-    let numberOfImpossibleThings = 10
-    let meal = "teatime"
+func impossibleThingsPhrase(numberOfImpossibleThings: Int, meal: String) -> String {
     return "Why, I've believed as many as \(numberOfImpossibleThings) before \(meal)"
 }
 //: - callout(Exercise): Update the `impossibleThingsPhrase` function so that, instead of using its two internal constants, it takes two arguments: `numberOfImpossibleThings` as an `Int` and `meal` as a `String`.
@@ -22,8 +20,8 @@ func impossibleThingsPhrase() -> String {
 //: Now you have two functions that take parameters and return values.
 //: - callout(Exercise): Call `impossibleBeliefsCount` and store the result in a constant.\
 //:Call `impossibleThingsPhrase`, passing in the result of `impossibleBeliefsCount` as one of the arguments.
-
-
+let impossibleBeliefs = impossibleBeliefsCount(pigsFlying: 50, frogsBecomingPrinces: 100, multipleLightningStrikes: 200)
+let impossibleThings = impossibleThingsPhrase(numberOfImpossibleThings: impossibleBeliefs, meal: "dinner")
 
 
 
