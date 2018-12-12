297ffad48c0c710a0dae6ff19608c351a10ed00f
David Dinh <david.dinh@medmen.com>
2018-12-12 00:36:48 -0800

    set questions from input and answers from struct

--- 16_QuestionBot2/ChatBot/ChatBot/ConversationDataSource.swift
+++ 16_QuestionBot2/ChatBot/ChatBot/ConversationDataSource.swift
@@ -3,15 +3,20 @@ class ConversationDataSource {
     
     /// The number of Messages in the conversation
     var messageCount = 0
+    var messages = [Message]()
     
     /// Add a new question to the conversation
     func add(question: String) {
+        let message = Message(date: Date(), text: question, type: .question)
+        messages.append(message)
         print("Asked to add question: \(question)")
         messageCount += 1
     }
     
     /// Add a new answer to the conversation
     func add(answer: String) {
+        let message = Message(date: Date(), text: answer, type: .answer)
+        messages.append(message)
         print("Asked to add answer: \(answer)")
          messageCount += 1
     }
@@ -19,10 +24,6 @@ class ConversationDataSource {
     /// The Message at a specific point in the conversation
     func messageAt(index: Int) -> Message {
         print("Asking for message at index \(index)")
-        if index % 2 == 0 {
-            return Message(date: Date(), text: "Question \(index / 2)", type: .question)
-        } else {
-            return Message(date: Date(), text: "Answer \(index / 2)", type: .answer)
-        }
+        return messages[index]
     }
 }
