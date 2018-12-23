25a85e3b9bb5b9b9fd323b690dca6a60316ad0fc
David Dinh <david.dinh@medmen.com>
2018-12-23 01:38:31 -0800

    created ColorMix project

---
+++ 17_ActionsAndOutlets/ColorMix/ColorMix/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by David Dinh on 12/23/18.
+//  Copyright © 2018 David Dinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        colorView.backgroundColor = .black
+    }
+
+    @IBOutlet weak var colorView: UIView!
+}
+
