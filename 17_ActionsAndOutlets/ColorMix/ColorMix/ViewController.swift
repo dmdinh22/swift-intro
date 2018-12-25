076a3b8b515bf4c604bfdef53f1ff0c7c821d953
David Dinh <david.dinh@medmen.com>
2018-12-25 01:21:29 -0800

    add method to toggle color dependent on switch put on

--- 17_ActionsAndOutlets/ColorMix/ColorMix/ViewController.swift
+++ 17_ActionsAndOutlets/ColorMix/ColorMix/ViewController.swift
@@ -20,11 +20,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func switchChanged(_ sender: UISwitch) {
-        if sender.isOn {
-            colorView.backgroundColor = .red
-        } else {
-            colorView.backgroundColor = .black
+        updateColor()
+    }
+
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+
+        if redSwitch.isOn {
+            red = 1
         }
+
+        if greenSwitch.isOn {
+            green = 1
+        }
+
+        if blueSwitch.isOn {
+            blue = 1
+        }
+
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+
+        colorView.backgroundColor = color
     }
 }
 
